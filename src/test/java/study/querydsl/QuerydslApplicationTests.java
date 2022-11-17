ab844b97593ae1db3528c23770e9ea929fabc618
Yonghwi Kim <yh0921k@gmail.com>
2022-11-17 13:30:00 +0900

    init: QueryDSL 학습을 위한 저장소 초기화
    README.md
    - 저장소의 기반이 되는 강의 내용 추가
    
    .gitignore
    - Git 추적 제외 파일 추가
    
    buile.gradle
    - H2 Database 의존성 버전 변경

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,11 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
