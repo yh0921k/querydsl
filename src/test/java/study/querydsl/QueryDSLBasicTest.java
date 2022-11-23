a275e72dfbf0c990788d557139869bbcaed27497
Yonghwi Kim <yh0921k@gmail.com>
2022-11-23 12:40:17 +0900

    add: Section 03. Case

--- src/test/java/study/querydsl/QueryDSLBasicTest.java
+++ src/test/java/study/querydsl/QueryDSLBasicTest.java
@@ -2,7 +2,8 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
-import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,7 +19,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import java.util.List;
 
-import static com.querydsl.jpa.JPAExpressions.*;
+import static com.querydsl.jpa.JPAExpressions.select;
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
@@ -312,11 +313,7 @@ public class QueryDSLBasicTest {
     List<Member> result =
         queryFactory
             .selectFrom(member)
-            .where(
-                member.age.in(
-                    select(memberSub.age)
-                        .from(memberSub)
-                        .where(memberSub.age.gt(10))))
+            .where(member.age.in(select(memberSub.age).from(memberSub).where(memberSub.age.gt(10))))
             .fetch();
 
     assertThat(result).extracting("age").containsExactly(20, 30, 40);
@@ -325,13 +322,70 @@ public class QueryDSLBasicTest {
   @Test
   public void selectSubQuery() {
     QMember memberSub = new QMember("memberSub");
-    List<Tuple> result = queryFactory
-        .select(member.username, select(memberSub.age.avg()).from(memberSub))
-        .from(member)
-        .fetch();
+    List<Tuple> result =
+        queryFactory
+            .select(member.username, select(memberSub.age.avg()).from(memberSub))
+            .from(member)
+            .fetch();
 
     for (Tuple tuple : result) {
       System.out.println("tuple = " + tuple);
+    }
+  }
+
+  @Test
+  public void basicCase() {
+    List<String> result =
+        queryFactory
+            .select(member.age.when(10).then("열살").when(20).then("스무살").otherwise("기타"))
+            .from(member)
+            .fetch();
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
+  }
+
+  @Test
+  public void complexCase() {
+    List<String> result =
+        queryFactory
+            .select(
+                new CaseBuilder()
+                    .when(member.age.between(0, 20))
+                    .then("0 ~ 20")
+                    .when(member.age.between(21, 30))
+                    .then("21 ~ 30")
+                    .otherwise("기타"))
+            .from(member)
+            .fetch();
+
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
+  }
+
+  @Test
+  public void orderByWithCase() {
+    NumberExpression<Integer> rankPath =
+        new CaseBuilder()
+            .when(member.age.between(0, 20))
+            .then(2)
+            .when(member.age.between(21, 30))
+            .then(1)
+            .otherwise(3);
+
+    List<Tuple> result =
+        queryFactory
+            .select(member.username, member.age, rankPath)
+            .from(member)
+            .orderBy(rankPath.desc())
+            .fetch();
+
+    for (Tuple tuple : result) {
+      String username = tuple.get(member.username);
+      Integer age = tuple.get(member.age);
+      Integer rank = tuple.get(rankPath);
+      System.out.println("username = " + username + " age = " + age + " rank = " + rank);
     }
   }
 }
