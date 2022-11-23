cfdfbaa6261cc06858a5c4bc87bcfd2a346d3f9f
Yonghwi Kim <yh0921k@gmail.com>
2022-11-23 12:48:10 +0900

    add: Section 03. 상수, 문자 더하기

--- src/test/java/study/querydsl/QueryDSLBasicTest.java
+++ src/test/java/study/querydsl/QueryDSLBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -386,6 +387,28 @@ public class QueryDSLBasicTest {
       Integer age = tuple.get(member.age);
       Integer rank = tuple.get(rankPath);
       System.out.println("username = " + username + " age = " + age + " rank = " + rank);
+    }
+  }
+
+  @Test
+  public void constant() {
+    List<Tuple> result =
+        queryFactory.select(member.username, Expressions.constant("A")).from(member).fetch();
+    for (Tuple tuple : result) {
+      System.out.println("tuple = " + tuple);
+    }
+  }
+
+  @Test
+  public void concat() {
+    List<String> result =
+        queryFactory
+            .select(member.username.concat("_").concat(member.age.stringValue()))
+            .from(member)
+            .fetch();
+
+    for (String s : result) {
+      System.out.println("s = " + s);
     }
   }
 }
