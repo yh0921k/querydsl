98318bdeb3b5f45906db8e6a4e1794c9344c5d9d
Yonghwi Kim <yh0921k@gmail.com>
2022-11-24 09:13:09 +0900

    add: Section 04. 프로젝션과 결과 반환(@QueryProjection)

--- src/test/java/study/querydsl/QueryDSLBasicTest.java
+++ src/test/java/study/querydsl/QueryDSLBasicTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -505,6 +506,16 @@ public class QueryDSLBasicTest {
             .fetch();
     for (UserDto userDto : result) {
       System.out.println("userDto = " + userDto);
+    }
+  }
+
+  @Test
+  public void findDtoByQueryProjection() {
+    List<MemberDto> result =
+        queryFactory.select(new QMemberDto(member.username, member.age)).from(member).fetch();
+
+    for (MemberDto memberDto : result) {
+      System.out.println("memberDto = " + memberDto);
     }
   }
 }
